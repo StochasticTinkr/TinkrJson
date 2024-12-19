6487b3f2aaac1956ccfaadf67db8895e424e717f
Daniel Pitts <git@stochastictinker.com>
2024-12-19 15:58:48 -0800

    Make JsonWalker.kt internal, add a little documentation to it.

--- src/main/kotlin/com/stochastictinkr/json/walker/JsonWalker.kt
+++ src/main/kotlin/com/stochastictinkr/json/walker/JsonWalker.kt
@@ -2,7 +2,10 @@ package com.stochastictinkr.json.walker
 
 import com.stochastictinkr.json.*
 
-class ElementStack(
+/**
+ * A helper data structure for depth-first traversal of a JSON structure, with cycle detection.
+ */
+internal class ElementStack(
     val element: JsonElement,
     val parent: ElementStack? = null,
 ) {
@@ -16,8 +19,32 @@ class ElementStack(
     private val ancestors: Sequence<JsonElement> = generateSequence(this) { it.parent }.map { it.element }
 }
 
-typealias ElementVisitor<R> = DeepRecursiveFunction<ElementStack, R>
+/**
+ * A [DeepRecursiveFunction] for depth-first traversal of a JSON structure.
+ *
+ * Example usage:
+ * ```
+ * val countNulls = ElementVisitor<Int> { stack ->
+ *     when (val element = stack.element) {
+ *       is JsonNull -> 1
+ *       is JsonObject -> stack.element.values.sumBy { callRecursive(stack.push(it)) }
+ *       is JsonArray -> stack.element.sumBy { callRecursive(stack.push(it)) }
+ *       else -> 0
+ *     }
+ * }
+ *
+ * fun example(element: JsonElement): Int {
+ *    println("There are ${countNulls(element)} nulls in the JSON structure.")
+ * }
+ * ```
+ */
+internal typealias ElementVisitor<R> = DeepRecursiveFunction<ElementStack, R>
+/**
+ * A [DeepRecursiveScope] for depth-first traversal of a JSON structure.
+ */
+internal typealias ElementVisitorScope<R> = DeepRecursiveScope<ElementStack, R>
 
-typealias ElementVisitorScope<R> = DeepRecursiveScope<ElementStack, R>
-
-operator fun <R> ElementVisitor<R>.invoke(element: JsonElement): R = this(ElementStack(element))
+/**
+ * Convenience function for invoking an [ElementVisitor] on the root [JsonElement].
+ */
+internal operator fun <R> ElementVisitor<R>.invoke(element: JsonElement): R = this(ElementStack(element))
